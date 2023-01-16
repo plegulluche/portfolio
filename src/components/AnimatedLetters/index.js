2f48963ad3c7bd9ebcc3ed3223e69ef6652b8a30
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2023-01-16 15:01:16 +0100

    [ADD] changing images in assets folder (logos without background ) adding animated letter component

---
+++ src/components/AnimatedLetters/index.js
@@ -0,0 +1,16 @@
+import './index.scss';
+
+// takes an array of letters and return the letters animated
+const AnimatedLetters = ({ letterClass, strArray, idx }) => {
+  return (
+    <span>
+      {strArray.map((char, i) => (
+        <span key={char + i} className={`${letterClass} _${i + idx}`}>
+          {char}
+        </span>
+      ))}
+    </span>
+  );
+};
+
+export default AnimatedLetters;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,64 @@
 import './index.scss';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/L.png';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = ['e', 'g', 'u', 'l', 'l', 'u', 'c', 'h', 'e'];
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+  ];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+  }, []);
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm Pierre <br />
-          <img src={LogoTitle} alt="developer" />e gulluche
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          Web developer
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <br />
+          <span className={`${letterClass} _15`}>P</span>
+
+          <span className={`${letterClass} _16`}>i</span>
+          <span className={`${letterClass} _17`}>e</span>
+          <span className={`${letterClass} _18`}>r</span>
+          <span className={`${letterClass} _19`}>r</span>
+          <span className={`${letterClass} _20`}>e</span>
+          <br />
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={23}
+          />
         </h1>
         <h2>Backend developer, apprentice Fullstack developer</h2>
         <Link to="/contact" className="flat-button">
