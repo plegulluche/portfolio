181e42fcd4bf07b14f356c4091e405a2c8cf60ed
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2023-01-18 19:19:30 +0100

    [ADD] adding a pacman loader for about and home page, adding a spinning cube on the about page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import LogoTitle from '../../assets/images/L.png';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from './Logo';
 import './index.scss';
+import Loader from 'react-loaders';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -31,43 +32,46 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <br />
-          <span className={`${letterClass} _15`}>P</span>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <br />
+            <span className={`${letterClass} _15`}>P</span>
 
-          <span className={`${letterClass} _16`}>i</span>
-          <span className={`${letterClass} _17`}>e</span>
-          <span className={`${letterClass} _18`}>r</span>
-          <span className={`${letterClass} _19`}>r</span>
-          <span className={`${letterClass} _20`}>e</span>
-          <br />
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={23}
-          />
-        </h1>
-        <h2>Backend developer, apprentice Fullstack developer</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+            <span className={`${letterClass} _16`}>i</span>
+            <span className={`${letterClass} _17`}>e</span>
+            <span className={`${letterClass} _18`}>r</span>
+            <span className={`${letterClass} _19`}>r</span>
+            <span className={`${letterClass} _20`}>e</span>
+            <br />
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={23}
+            />
+          </h1>
+          <h2>Backend developer, apprentice Fullstack developer</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
