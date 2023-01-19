4f544fa1459e7eccd208b9b185d88538fa74f9ce
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2023-01-19 10:15:44 +0100

    [ADD] adding portfolio component , creating route in app.js for portfolio, creating dummy compo in Component/index.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
 import Layout from './components/Layout';
 import Home from './components/Home';
 
@@ -13,6 +14,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
+          <Route path="portfolio" element={<Portfolio />} />
         </Route>
       </Routes>
     </>
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,5 @@
+import './index.scss';
+
+const Portfolio = () => 'hello';
+
+export default Portfolio;
