b6b954cc89b9776540b51c0d5666d1d48308e09e
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2023-01-21 14:21:05 +0100

    [ADD] adding title , and method to diplay project on portfolio page with images , adding somestyling in layout.scss and portfolio.scss

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,8 +1,57 @@
-import React from 'react';
-import './index.scss';
+import React, { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+import portfolioData from "../../data/portfolio.json";
 
 const Portfolio = () => {
-  return <div className="container portfolio-page"></div>;
+  const [letterClass, setLetterClass] = useState("text-animate");
+  console.log(portfolioData);
+
+  //Hover effect on title
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={port.cover}
+                className="portfolio-image"
+                alt="portfolio"
+              />
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={"Portfolio".split("")}
+            idx={15}
+          />
+        </h1>
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
 };
 
 export default Portfolio;
